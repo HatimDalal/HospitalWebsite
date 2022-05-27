aa272b716cb58a257db085f78ae882ebc20c278c
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-27 20:36:08 +0530

    header

---
+++ src/app/OurServices/OurServices.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-header',
+  templateUrl:'./OurServices.component.html',
+  styleUrls: ['./OurServices.component.css']
+})
+
+export class OurServicesComponent{}
