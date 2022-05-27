44f89d5009c4199ae11ebaa9bb466ce3d5344306
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-27 21:20:04 +0530

    hh

--- src/app/OurServices/OurServices.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector:'app-header',
-  templateUrl:'./OurServices.component.html',
-  styleUrls: ['./OurServices.component.css']
-})
-
-export class OurServicesComponent{}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[]
 })
 export class AppRoutingModule { }
+
+
+
+
+
+
