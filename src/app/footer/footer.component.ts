55591d8bd9d578bd505c7653eb50578db3eff270
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-27 15:58:43 +0530

    change

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,25 +1,9 @@
 import { Component } from "@angular/core";
 
-export interface PeriodicElement {
-  ourservices: string;
-  getintouch: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {ourservices: 'Patient Care', getintouch: '022-3456 3456'},
-  {ourservices: 'Medical Services', getintouch: '022-4567 4567'},
-  {ourservices: 'Surgical Services', getintouch: '022-7896 7896'},
-  {ourservices: 'cancer Care', getintouch: ' HospitalWebsite@gmail.com'},
-  {ourservices: 'OPD Services', getintouch: ' HospitalQuery@gmail.com'},
-];
-
 @Component({
   selector:'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 
-export class footerComponent{
-  displayedColumns: string[] = ['ourservices','getintouch'];
-  dataSource = ELEMENT_DATA;
-}
+export class footerComponent{}
