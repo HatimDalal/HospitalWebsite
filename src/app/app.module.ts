3c6ef543301c7eaacae5e8cec83ef23268ffec43
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-27 14:57:14 +0530

    change2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { footerComponent } from './footer/footer.component';
+
 
 import { FormsModule } from "@angular/forms";
 
