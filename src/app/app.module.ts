479c4ef145b7785fb82b3bacceef280d0d149c6a
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-27 14:20:59 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { footerComponent } from './footer/footer.component';
 
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    footerComponent
   ],
   imports: [
     BrowserModule,
