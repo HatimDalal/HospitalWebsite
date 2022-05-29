2e3998e4cd673f820a7deca3b3206e5c6cc2ae69
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-29 18:21:55 +0530

    spacing

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class AboutUSComponent implements OnInit {
 
   image:string="assets/images/doctor4.svg";
-  image1:string="assets/images/istockphoto.jpg";
+  image1:string="assets/images/istockphoto.svg";
   image2:string="assets/images/doctor3.jpg";
   image3:string="assets/images/doctor1.jpg";
-  image4:string="assets/images/best-surgeons.jpg";
+  image4:string="assets/images/best-surgeons.svg";
   image5:string="assets/images/doctors.svg";
   image6:string="assets/images/hospital-design.svg";
 
