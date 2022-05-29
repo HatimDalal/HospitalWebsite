442da4ad1b71b04125bb65077d1aea418b86f492
Garima vashishtha <garima15580@mastek.com>
2022-05-29 16:29:03 +0530

    garimafive

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AboutUSComponent implements OnInit {
 
   image:string="assets/images/doctor4.svg";
-  image1:string="assets/images/doctor2.jpg";
+  image1:string="assets/images/istockphoto.jpg";
   image2:string="assets/images/doctor3.jpg";
   image3:string="assets/images/doctor1.jpg";
+  image4:string="assets/images/best-surgeons.jpg";
+  image5:string="assets/images/doctors.svg";
+  image6:string="assets/images/hospital-design.svg";
 
   constructor() { }
 
