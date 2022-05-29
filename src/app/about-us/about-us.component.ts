2c0545baeff0016e91c4bfb73507ec838e722ba8
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-29 18:26:23 +0530

    garima

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -7,13 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUSComponent implements OnInit {
 
-  image:string="assets/images/doctor4.svg";
   image1:string="assets/images/istockphoto.svg";
-  image2:string="assets/images/doctor3.jpg";
-  image3:string="assets/images/doctor1.jpg";
-  image4:string="assets/images/best-surgeons.svg";
-  image5:string="assets/images/doctors.svg";
-  image6:string="assets/images/hospital-design.svg";
+  image2:string="assets/images/best-surgeons.svg";
 
   constructor() { }
 
