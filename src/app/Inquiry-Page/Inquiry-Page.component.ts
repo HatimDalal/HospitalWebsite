7673a351c625846b0972895cda06020ffc8894f3
Geetanjali Dhoke <gorgeousgeetanjali15@gmail.com>
2022-05-30 23:31:50 +0530

    InquiryForm

--- src/app/Inquiry-Page/Inquiry-Page.component.ts
+++ src/app/Inquiry-Page/Inquiry-Page.component.ts
@@ -1,8 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-ourservices',
   templateUrl: './Inquiry-Page.component.html',
   styleUrls: ['./Inquiry-Page.component.css']
 })
-export class InquiryPageComponent{}
+export class InquiryPageComponent{
+  inquiryform!:FormGroup
+  submitted = false;
+
+  constructor(private FormBuilder:FormBuilder){}
+
+  ngOnInit(){
+    //validations
+    this.inquiryform = this.FormBuilder.group({
+      firstName:['',Validators.required],
+      lastName:['',Validators.required],
+      Number:['',[Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
+      Email:['',[Validators.required,Validators.email]],
+      Query:['',Validators.required]
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true
+
+    if(this.inquiryform.invalid){
+      return
+    }
+    alert("Success")
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,6 +35,7 @@ import { RouterModule } from "@angular/router";
     AppRoutingModule,
     FormsModule,
     NgbModule,
+    ReactiveFormsModule,
 
     RouterModule.forRoot([
       {path: '', component:HomeComponent},
