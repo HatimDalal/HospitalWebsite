f0511cbc8d24135020d4ad1315236a6aec9f93bb
Garima vashishtha <garima15580@mastek.com>
2022-06-02 14:56:15 +0530

    bookservice

---
+++ src/app/booking.ts
@@ -0,0 +1,12 @@
+export interface Booking {
+    firstname:string,
+    lastname:string,
+    Phone:number,
+    Email:string
+    Age:number,
+    Bgroup:number,
+    doctorname:string,
+    message:string,
+
+
+}
--- src/app/free-checkup/free-checkup.component.ts
+++ src/app/free-checkup/free-checkup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import party from "party-js";
+
 @Component({
   selector: 'app-free-checkup',
   templateUrl: './free-checkup.component.html',
